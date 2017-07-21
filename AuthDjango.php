15728e85880203c27abcf153cb5b8d0e3773e487
David Svenson <davidsvenson@outlook.com>
2017-07-21 19:40:54 +0200

    Use PostgreSQL instead of MySQL

---
+++ AuthDjango.php
@@ -0,0 +1,40 @@
+<?php
+// Django authentication plugin.
+// https://bitbucket.org/pythonian4000/django-mediawiki-authentication-by-user-id/
+
+// Configuration variables
+$wgAuthDjangoConfig = array();
+
+$wgAuthDjangoConfig['DjangoHost']   = 'localhost';   // Django PostgreSQL Host Name.
+$wgAuthDjangoConfig['DjangoPort']   = '5432';	     // Django PostgreSQL port
+$wgAuthDjangoConfig['DjangoUser']   = 'mediawiki';   // Django PostgreSQL Username.
+$wgAuthDjangoConfig['DjangoPass']   = 'N03%uD!E1o*X!10C';  // Django PostgreSQL Password.
+$wgAuthDjangoConfig['DjangoDBName'] = 'megabeta';    // Django PostgreSQL Database Name.
+
+$wgAuthDjangoConfig['AuthDjangoTable']      = 'authdjango';
+$wgAuthDjangoConfig['UserTable']            = 'auth_user';
+$wgAuthDjangoConfig['SessionTable']         = 'django_session';
+$wgAuthDjangoConfig['SessionprofileTable']  = 'wiki_sessionprofile';
+
+$wgAuthDjangoConfig['LinkToSiteLogin']       = 'https://megabeta.se/'; //Django site login URL
+$wgAuthDjangoConfig['LinkToWiki']            = '/wiki/'; //Subdirectory wiki
+
+// Load classes
+$wgAutoloadClasses['AuthPlugin'] = dirname('./include') . '/AuthPlugin.php';
+$wgAutoloadClasses['AuthDjango'] = dirname(__FILE__) . '/AuthDjango.body.php';
+
+// Schema updates for update.php
+
+$wgHooks['LoadExtensionSchemaUpdates'][] = 'addAuthDjangoTables';
+function addAuthDjangoTables() {
+    global $wgExtNewTables;
+    $wgExtNewTables[] = array(
+        'authdjango',
+        dirname( __FILE__ ) . '/tables-authdjango.sql' );
+    return true;
+}
+
+$wgExtensionFunctions[] = "initAuthDjango";
+function initAuthDjango() {
+    $wgAuth = new AuthDjango();     // Initiate Auth Plugin
+}
